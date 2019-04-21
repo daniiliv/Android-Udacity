ce7b16f4570aacfe0e2a960ee621df84570b5564
daniiliv <dania-spb@mail.ru>
2019-04-21 19:46:03 +0300

    exS 4.2. pass a forecast to a detailed activity from the main activity.

--- ud851-Sunshine-student/S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ ud851-Sunshine-student/S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,16 +1,31 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    TextView mDetailedWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        mDetailedWeather = findViewById(R.id.tv_detailed_weather);
+
+        Intent intentThatStartedThisActivity = getIntent();
+
+        if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
+
+            String weatherForDay = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+            mDetailedWeather.setText(weatherForDay);
+        }
+
+
 
         // TODO (2) Display the weather forecast that was passed from MainActivity
     }
