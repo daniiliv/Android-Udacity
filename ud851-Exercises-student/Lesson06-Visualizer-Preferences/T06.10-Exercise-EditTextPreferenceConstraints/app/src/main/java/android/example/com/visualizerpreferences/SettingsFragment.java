6818aa7b507e983ed40feb47cbf993b12a02928b
daniiliv <dania-spb@mail.ru>
2019-05-03 12:30:01 +0300

    exT 6.10. Constraint EditTextPreference (NumberFormatException fixed).

--- ud851-Exercises-student/Lesson06-Visualizer-Preferences/T06.10-Exercise-EditTextPreferenceConstraints/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ ud851-Exercises-student/Lesson06-Visualizer-Preferences/T06.10-Exercise-EditTextPreferenceConstraints/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -29,7 +29,7 @@ import android.widget.Toast;
 
 // TODO (1) Implement OnPreferenceChangeListener
 public class SettingsFragment extends PreferenceFragmentCompat implements
-        OnSharedPreferenceChangeListener {
+        OnSharedPreferenceChangeListener, Preference.OnPreferenceChangeListener {
 
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
@@ -52,6 +52,9 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
             }
         }
         // TODO (3) Add the OnPreferenceChangeListener specifically to the EditTextPreference
+        Preference preference = findPreference(getString(R.string.pref_size_key));
+
+        preference.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -105,5 +108,29 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
         super.onDestroy();
         getPreferenceScreen().getSharedPreferences()
                 .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Toast errorToast = Toast.makeText(getContext(), "Please select a number between 0.1 and 3", Toast.LENGTH_SHORT);
+
+        String sizeKey = getString(R.string.pref_size_key);
+
+        if (preference.getKey().equals(sizeKey)) {
+            String stringSizeValue = (String) newValue;
+
+            try {
+                float size = Float.parseFloat(stringSizeValue);
+                if (size > 3 || size <= 0) {
+                    errorToast.show();
+                    return false;
+                }
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                errorToast.show();
+                return false;
+            }
+        }
+        return true;
     }
 }
