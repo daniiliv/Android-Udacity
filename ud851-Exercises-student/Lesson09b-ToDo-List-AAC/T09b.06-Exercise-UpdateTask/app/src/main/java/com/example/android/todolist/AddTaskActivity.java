778c55a4371bd8ca0a4de308e342fbd41e97be48
daniiliv <dania-spb@mail.ru>
2019-06-16 18:43:17 +0300

    T9b.6. Update task. Pass an id of the entry through intent. Populate UI when updating task.
    SELECT ... WHERE id.

--- ud851-Exercises-student/Lesson09b-ToDo-List-AAC/T09b.06-Exercise-UpdateTask/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
+++ ud851-Exercises-student/Lesson09b-ToDo-List-AAC/T09b.06-Exercise-UpdateTask/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
@@ -71,17 +71,24 @@ public class AddTaskActivity extends AppCompatActivity {
             mButton.setText(R.string.update_button);
             if (mTaskId == DEFAULT_TASK_ID) {
                 // populate the UI
-                // TODO (3) Assign the value of EXTRA_TASK_ID in the intent to mTaskId
-                // Use DEFAULT_TASK_ID as the default
+                mTaskId = intent.getIntExtra(EXTRA_TASK_ID, DEFAULT_TASK_ID);
 
-                // TODO (4) Get the diskIO Executor from the instance of AppExecutors and
-                // call the diskIO execute method with a new Runnable and implement its run method
+                AppExecutors.getInstance().diskIO().execute(new Runnable() {
 
-                // TODO (5) Use the loadTaskById method to retrieve the task with id mTaskId and
-                // assign its value to a final TaskEntry variable
+                    @Override
+                    public void run() {
+                        final TaskEntry taskEntry = mDb.taskDao().loadTaskById(mTaskId);
+                        // We will be able to simplify this once we learn more
+                        // about Android Architecture Components
+                        runOnUiThread(new Runnable() {
 
-                // TODO (6) Call the populateUI method with the retrieve tasks
-                // Remember to wrap it in a call to runOnUiThread
+                            @Override
+                            public void run() {
+                                populateUI(taskEntry);
+                            }
+                        });
+                    }
+                });
             }
         }
     }
@@ -114,9 +121,13 @@ public class AddTaskActivity extends AppCompatActivity {
      * @param task the taskEntry to populate the UI
      */
     private void populateUI(TaskEntry task) {
-        // TODO (7) return if the task is null
 
-        // TODO (8) use the variable task to populate the UI
+        if (task == null) {
+            return;
+        } else {
+            mEditText.setText(task.getDescription());
+            setPriorityInViews(task.getPriority());
+        }
     }
 
     /**
@@ -135,7 +146,12 @@ public class AddTaskActivity extends AppCompatActivity {
                 // TODO (9) insert the task only if mTaskId matches DEFAULT_TASK_ID
                 // Otherwise update it
                 // call finish in any case
-                mDb.taskDao().insertTask(taskEntry);
+                if (mTaskId == DEFAULT_TASK_ID) {
+                    mDb.taskDao().insertTask(taskEntry);
+                } else {
+                    taskEntry.setId(mTaskId);
+                    mDb.taskDao().updateTask(taskEntry);
+                }
                 finish();
             }
         });
